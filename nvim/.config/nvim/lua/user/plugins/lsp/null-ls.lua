d526ece42cd0dd97d60d234e429ac144c0927e19
magoz <apps@magoz.is>
2022-06-26 09:32:44 +0200

    remove unused formatters and add a comment regarding checkhealth error

--- nvim/.config/nvim/lua/user/plugins/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/null-ls.lua
@@ -8,17 +8,17 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
--- https://github.com/prettier-solidity/prettier-plugin-solidity
+-- For now, ignore following error message when runing :checkhealth 
+-- ERROR: stylua: the command "stylua" is not executable.
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/912
+
 null_ls.setup {
-  debug = false,
+  debug = true,
   sources = {
     formatting.prettier.with {
       extra_filetypes = { "toml" },
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
-    formatting.black.with { extra_args = { "--fast" } },
     formatting.stylua,
-    formatting.google_java_format,
-    diagnostics.flake8,
   },
 }
