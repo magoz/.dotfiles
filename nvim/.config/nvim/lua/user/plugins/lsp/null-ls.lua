3123fdbb12276e1fd4464f6a080370d818c7e3c1
magoz <apps@magoz.is>
2022-06-26 17:29:54 +0200

    remove old comment and other small fixes

--- nvim/.config/nvim/lua/user/plugins/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/null-ls.lua
@@ -2,18 +2,13 @@
 -- https://github.com/LunarVim/nvim-basic-ide/blob/master/lua/user/lsp/null-ls.lua
 -- https://www.youtube.com/watch?v=b7OguLuaYvE
 
-
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
   return
 end
 
 local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
-
--- For now, ignore following error message when runing :checkhealth
--- ERROR: stylua: the command "stylua" is not executable.
--- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/912
+-- local diagnostics = null_ls.builtins.diagnostics
 
 -- Reference
 -- Formatting: https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins
