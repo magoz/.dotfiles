47dd1deba393223897f4b2eaecc19fdddd6f1e76
magoz <apps@magoz.is>
2022-07-03 12:56:00 +0200

    fix require cssls

--- nvim/.config/nvim/lua/user/plugins/lsp/servers/cssls.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/servers/cssls.lua
@@ -4,7 +4,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 M.setup = function(on_attach)
-	require("lspconfig").cssmodules_ls.setup({
+	require("lspconfig").cssls.setup({
 		on_attach = on_attach,
 		capabilities = capabilities,
 	})
