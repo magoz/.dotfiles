f22a5f6e7670ece330d0d64c2fa9116c0ad127bb
magoz <apps@magoz.is>
2022-07-08 12:35:29 +0200

    add which key remaps to telescope

--- nvim/.config/nvim/lua/user/plugins/telescope.lua
+++ nvim/.config/nvim/lua/user/plugins/telescope.lua
@@ -1,49 +1,57 @@
+local wk = require("which-key")
+
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-  return
+	return
 end
 
-local actions = require "telescope.actions"
+local actions = require("telescope.actions")
 
-telescope.setup {
-  defaults = {
+telescope.setup({
+	defaults = {
 
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
-    file_ignore_patterns = { ".git/", "node_modules" },
+		prompt_prefix = " ",
+		selection_caret = " ",
+		path_display = { "smart" },
+		file_ignore_patterns = { ".git/", "node_modules" },
 
-    mappings = {
-      i = {
-        ["<Down>"] = actions.cycle_history_next,
-        ["<Up>"] = actions.cycle_history_prev,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-      },
-    },
+		mappings = {
+			i = {
+				["<Down>"] = actions.cycle_history_next,
+				["<Up>"] = actions.cycle_history_prev,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+			},
+		},
 
-     vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-      '-uu' -- search hidden files
-    },
-
-  },
-}
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"-uu", -- search hidden files
+		},
+	},
+})
 
 -- ---------------------------------
 -- ----------- REMAPS --------------
 -- ---------------------------------
-local keymap = vim.keymap.set
-local opts = { silent = true }
+wk.register({
+	f = {
+		name = "Telescope", -- group name
+		f = { ":Telescope find_files hidden=true <CR>", "Search files" },
+		t = { ":Telescope live_grep<CR>", "Search inside files" },
+		b = { ":Telescope buffers<CR>", "Search Buffers" },
+		p = { ":Telescope projects<CR>", "Search Projects" },
+	},
+}, { prefix = "<leader>" })
 
--- keymap("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files()<cr>", opts)
-keymap("n", "<leader>ff", ":Telescope find_files hidden=true <CR>", opts)
-keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
-keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
-keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+-- -- keymap("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files()<cr>", opts)
+-- keymap("n", "<leader>ff", ":Telescope find_files hidden=true <CR>", opts)
+-- keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
+-- keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+-- keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
