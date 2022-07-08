1f75293f0d2c750b56caa585e186912ee015aa00
magoz <apps@magoz.is>
2022-07-08 12:45:01 +0200

    formatting, and use which-key for keymaps

--- nvim/.config/nvim/lua/user/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/user/plugins/toggleterm.lua
@@ -1,3 +1,5 @@
+local wk = require("which-key")
+
 local status_ok, toggleterm = pcall(require, "toggleterm")
 if not status_ok then
 	return
@@ -21,15 +23,15 @@ toggleterm.setup({
 })
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+	local opts = { noremap = true }
+	-- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
@@ -38,12 +40,20 @@ function _LAZYGIT_TOGGLE()
 	lazygit:toggle()
 end
 
-
 -- ---------------------------------
 -- ----------- REMAPS --------------
 -- ---------------------------------
-local keymap = vim.keymap.set
-local opts = { silent = true }
+wk.register({
+	gg = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Toggle Lazy Git" },
+	-- g = {
+	-- 	name = "Go to..", -- group name
+	-- 	r = { ":LspReferences<CR>", "Show References" },
+	-- 	d = { ":LspDefinition<CR>", "Go to Definition" },
+	-- 	D = { ":LspDeclaration<CR>", "Go to Declaration (not supported in ts/js/css)" },
+	-- 	t = { ":LspTypeDefinition<CR>", "Go to Type Definition" },
+	-- 	a = { ":LspAct<CR>", "LSP Act" }, -- Not sure what this does
+	-- 	A = { "<Esc><cmd> LspRangeAct<CR>", "Actions (extract code, move to file, etc)", mode = "v" },
+	-- },
+}, { prefix = "<leader>" })
 
-keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
-
+-- keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
