9cc68a1c29d1161e764c75274a39d83d58216086
magoz <apps@magoz.is>
2022-09-18 21:31:56 +0200

    add margins

--- nvim/.config/nvim/lua/user/plugins/true-zen.lua
+++ nvim/.config/nvim/lua/user/plugins/true-zen.lua
@@ -5,7 +5,22 @@ if not status_ok then
 	return
 end
 
-true_zen.setup()
+true_zen.setup({
+	modes = { -- configurations per mode
+		ataraxis = {
+			minimum_writing_area = { -- minimum size of main window
+				width = 80,
+				height = 44,
+			},
+			padding = { -- padding windows
+				left = 100,
+				right = 100,
+				top = 70,
+				bottom = 70,
+			},
+		},
+	},
+})
 
 -- ---------------------------------
 -- ----------- REMAPS --------------
