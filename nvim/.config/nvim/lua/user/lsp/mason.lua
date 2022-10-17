b9ccb7f77b822c315bc1f51af9408bf28a09aad1
magoz <apps@magoz.is>
2022-10-17 22:01:36 +0200

    pcall mason

--- nvim/.config/nvim/lua/user/lsp/mason.lua
+++ nvim/.config/nvim/lua/user/lsp/mason.lua
@@ -24,7 +24,12 @@ local settings = {
 	max_concurrent_installers = 4,
 }
 
-require("mason").setup(settings)
+local status_ok, mason = pcall(require, "mason")
+if not status_ok then
+	return
+end
+
+mason.setup(settings)
 require("mason-lspconfig").setup({
 	ensure_installed = servers,
 	automatic_installation = true,
@@ -51,9 +56,4 @@ for _, server in pairs(servers) do
 	end
 
 	lspconfig[server].setup(opts)
-end
-local status_ok, mason = pcall(require, "mason")
-
-if not status_ok then
-	return
 end
