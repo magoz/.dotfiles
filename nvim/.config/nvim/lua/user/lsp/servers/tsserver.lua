dce4b533a0be2a1f2f4249efda87332ea687b503
magoz <apps@magoz.is>
2022-11-07 20:28:57 +0100

    fix tsserver whichkeys

--- nvim/.config/nvim/lua/user/lsp/servers/tsserver.lua
+++ nvim/.config/nvim/lua/user/lsp/servers/tsserver.lua
@@ -6,17 +6,6 @@ M.setup = function(on_attach, capabilities)
 	require("typescript").setup({
 		server = {
 			on_attach = function(client, bufnr)
-				wk.register({
-					t = {
-						name = "Typescript", -- optional group name
-						i = { ":TypescriptAddMissingImports<CR>", "Add missing imports" },
-						I = { ":TypescriptRemoveUnused<CR>", "Remove unused imports" },
-						o = { ":TypescriptOrganizeImports<CR>", "Organize imports" },
-						f = { ":TypescriptFixAll<CR>", "Fix all issues" },
-						r = { ":TypescriptRenameFile<CR>", "Rename file" },
-					},
-				}, { prefix = "<leader>" })
-
 				on_attach(client, bufnr)
 			end,
 			capabilities = capabilities,
@@ -24,4 +13,14 @@ M.setup = function(on_attach, capabilities)
 	})
 end
 
+wk.register({
+	t = {
+		name = "Typescript", -- optional group name
+		i = { ":TypescriptAddMissingImports<CR>", "Add missing imports" },
+		I = { ":TypescriptRemoveUnused<CR>", "Remove unused imports" },
+		o = { ":TypescriptOrganizeImports<CR>", "Organize imports" },
+		f = { ":TypescriptFixAll<CR>", "Fix all issues" },
+		r = { ":TypescriptRenameFile<CR>", "Rename file" },
+	},
+}, { prefix = "<leader>" })
 return M
