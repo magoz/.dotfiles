e5c62721beea238ffd0bd884fab3431f283b9099
magoz <apps@magoz.is>
2022-11-14 08:56:12 +0100

    add telescope-fzf-native.nvim

--- nvim/.config/nvim/lua/user/plugins/telescope.lua
+++ nvim/.config/nvim/lua/user/plugins/telescope.lua
@@ -69,6 +69,10 @@ telescope.setup({
 	},
 })
 
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+telescope.load_extension("fzf")
+
 -- ---------------------------------
 -- ----------- REMAPS --------------
 -- ---------------------------------
