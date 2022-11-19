6bc47e3997222e28095796a0a428d5819b3da85c
magoz <apps@magoz.is>
2022-11-19 13:36:16 +0100

    fix packer

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -96,10 +96,11 @@ return packer.startup({
 
 		-- Telescope
 		use({ "nvim-telescope/telescope.nvim" })
-		use({
-			"nvim-telescope/telescope-fzf-native.nvim",
-			run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-		})
+		use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+		-- use({
+		-- 	"nvim-telescope/telescope-fzf-native.nvim",
+		-- 	run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+		-- })
 
 		-- Treesitter
 		use({ "nvim-treesitter/nvim-treesitter" })
