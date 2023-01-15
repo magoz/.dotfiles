2a0d85db9668423dda91a701fc2ca90e20ecac91
magoz <apps@magoz.is>
2023-01-15 20:12:39 +0100

    explicitly define treesitter languages and add incremental selection

--- nvim/.config/nvim/lua/user/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/user/plugins/treesitter.lua
@@ -4,8 +4,45 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
+	ensure_installed = {
+		"help",
+		"vim",
+		"lua",
+		"javascript",
+		"typescript",
+		"tsx",
+		"jsdoc",
+		"prisma",
+		"css",
+		"scss",
+		"html",
+		"sql",
+		"gitignore",
+		"json",
+		"json5",
+		"jsonc",
+		"markdown",
+		"markdown_inline",
+		"dockerfile",
+		"regex",
+		"bash",
+		"make",
+		"yaml",
+		"toml",
+		"glsl",
+		"rust",
+		"query",
+	},
 	highlight = {
 		enable = true, -- false will disable the whole extension
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<c-n>",
+			node_incremental = "<c-n>",
+			node_decremental = "<c-p>",
+			-- scope_incremental = "<c-s>",
+		},
 	},
 })
