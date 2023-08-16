badf7876466d917a7a05aec4e0c22ae886533442
magoz <apps@magoz.is>
2023-08-16 13:57:43 +0200

    show more path

--- nvim/.config/nvim/lua/user/plugins/telescope.lua
+++ nvim/.config/nvim/lua/user/plugins/telescope.lua
@@ -12,7 +12,7 @@ telescope.setup({
 
 		prompt_prefix = " ",
 		selection_caret = " ",
-		path_display = { "smart" },
+		path_display = { truncate = 4 },
 
 		file_ignore_patterns = {
 			".DS_Store",
