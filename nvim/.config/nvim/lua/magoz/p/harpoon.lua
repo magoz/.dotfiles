89b7c2bfa73e2b0fc60f997676f4c8807d92bd19
magoz <apps@magoz.is>
2023-12-20 23:22:57 +0100

    add harpoon numbered keymaps

--- nvim/.config/nvim/lua/magoz/p/harpoon.lua
+++ nvim/.config/nvim/lua/magoz/p/harpoon.lua
@@ -30,5 +30,29 @@ return {
 			"<cmd>lua require('harpoon.ui').nav_prev()<cr>",
 			{ desc = "Go to previous harpoon mark" }
 		)
+		vim.keymap.set(
+			"n",
+			"<leader>h1",
+			"<cmd>lua require('harpoon.ui').nav_file(1)<cr>",
+			{ desc = "Go to harpoon mark 1" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>h2",
+			"<cmd>lua require('harpoon.ui').nav_file(2)<cr>",
+			{ desc = "Go to harpoon mark 2" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>h3",
+			"<cmd>lua require('harpoon.ui').nav_file(3)<cr>",
+			{ desc = "Go to harpoon mark 3" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>h4",
+			"<cmd>lua require('harpoon.ui').nav_file(4)<cr>",
+			{ desc = "Go to harpoon mark 4" }
+		)
 	end,
 }
