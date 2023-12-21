bff38ed441e99c116066b5127b70985381289bed
magoz <apps@magoz.is>
2023-12-21 12:40:08 +0100

    wip adjust new config

--- nvim/.config/nvim/lua/magoz/p/indent-blankline.lua
+++ nvim/.config/nvim/lua/magoz/p/indent-blankline.lua
@@ -1,17 +1,35 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
 	opts = {
-		show_current_context = true,
-		indent_blankline_char = "▏",
-		indent_blankline_show_trailing_blankline_indent = false,
-		indent_blankline_show_first_indent_level = true,
-		indent_blankline_use_treesitter = true,
-		indent_blankline_show_current_context = true,
-		indent_blankline_buftype_exclude = { "terminal", "nofile" },
-		indent_blankline_filetype_exclude = {
-			"help",
-			"packer",
-			"NvimTree",
+		indent = {
+			-- char = "▏",
+			-- highlight = {
+			-- 	"CursorColumn",
+			-- 	"Whitespace",
+			-- },
 		},
+		exclude = {
+			filetypes = {
+				"help",
+				"NvimTree",
+			},
+			buftypes = {
+				"terminal",
+				"nofile",
+			},
+		},
+		-- show_current_context = true,
+		-- _blankline_char = "▏",
+		-- indent_blankline_show_trailing_blankline_indent = false,
+		-- indent_blankline_show_first_indent_level = true,
+		-- indent_blankline_use_treesitter = true,
+		-- indent_blankline_show_current_context = true,
+		-- indent_blankline_buftype_exclude = { "terminal", "nofile" },
+		-- indent_blankline_filetype_exclude = {
+		-- 	"help",
+		-- 	"packer",
+		-- 	"NvimTree",
+		-- },
 	},
 }
