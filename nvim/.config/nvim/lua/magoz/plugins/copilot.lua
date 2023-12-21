458032314ba7b4375dd315ee4bea39716e6f0335
magoz <apps@magoz.is>
2023-12-21 21:05:24 +0100

    add copilot and other improvements

--- nvim/.config/nvim/lua/magoz/plugins/copilot.lua
+++
@@ -1,7 +0,0 @@
--- Node 18 is not supported yet, so we are pointing to node 16 installed via brew, which is not the default
-vim.g.copilot_node_command = "/usr/local/Cellar/node@16/16.16.0/bin/node"
-
--- Tab is used by cmp so we get the following error:
--- Copilot: <Tab> map has been disabled or is claimed by another plugin
--- This commands bypases the error, and we get no conflicts.
-vim.g.copilot_assume_mapped = true
