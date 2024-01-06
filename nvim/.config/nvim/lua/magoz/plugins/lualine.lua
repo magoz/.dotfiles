5ecea07c68fc3675a067e47ee35078a8cd7f4f24
magoz <apps@magoz.is>
2024-01-06 11:55:48 +0100

    minor improvements

--- nvim/.config/nvim/lua/magoz/plugins/lualine.lua
+++ nvim/.config/nvim/lua/magoz/plugins/lualine.lua
@@ -18,6 +18,26 @@ return {
 			always_visible = true,
 		}
 
+		local filename = {
+			"filename",
+			file_status = true, -- Displays file status (readonly status, modified status)
+			newfile_status = false, -- Display new file status (new file means no write after created)
+			path = 4, -- 0: Just the filename
+			-- 1: Relative path
+			-- 2: Absolute path
+			-- 3: Absolute path, with tilde as the home directory
+			-- 4: Filename and parent dir, with tilde as the home directory
+
+			shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+			-- for other components. (terrible name, any suggestions?)
+			symbols = {
+				modified = "[+]", -- Text to show when the file is modified.
+				readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+				unnamed = "[No Name]", -- Text to show for unnamed buffers.
+				newfile = "[New]", -- Text to show for newly created file before first write
+			},
+		}
+
 		local location = {
 			"location",
 			padding = 0,
@@ -78,8 +98,8 @@ return {
 				always_divide_middle = true,
 			},
 			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "filename" },
+				lualine_a = { "branch" },
+				lualine_b = { filename },
 				lualine_c = { diagnostics },
 				lualine_x = { copilot, "diff" },
 				lualine_y = { location },
