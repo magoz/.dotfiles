3213d52afd79f65c72281fca3370201f8770e003
magoz <apps@magoz.is>
2024-01-06 10:33:33 +0100

    fix icons

--- nvim/.config/nvim/lua/magoz/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/magoz/plugins/nvim-tree.lua
@@ -61,10 +61,10 @@ return {
 				enable = true,
 				show_on_dirs = true,
 				icons = {
-					hint = "",
-					info = "",
-					warning = "",
-					error = "",
+					hint = "󰌶",
+					info = "",
+					warning = "",
+					error = "",
 				},
 			},
 			view = {
