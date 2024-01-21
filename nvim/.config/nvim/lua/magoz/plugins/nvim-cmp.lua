14e411dd9ea2cf873bad9d29073c139c2e9335ba
magoz <apps@magoz.is>
2024-01-21 10:32:16 +0100

    minor improvements

--- nvim/.config/nvim/lua/magoz/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/magoz/plugins/nvim-cmp.lua
@@ -24,9 +24,9 @@ return {
 		local cmp = require("cmp")
 		local lspkind = require("lspkind")
 
-		local check_backspace = function()
-			local col = vim.fn.col(".") - 1
-			return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+		local function has_words_before()
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 		end
 
 		cmp.setup({
@@ -64,8 +64,8 @@ return {
 						luasnip.expand()
 					elseif luasnip.expand_or_jumpable() then
 						luasnip.expand_or_jump()
-					elseif check_backspace() then
-						fallback()
+					elseif has_words_before() then
+						cmp.complete()
 					else
 						fallback()
 					end
