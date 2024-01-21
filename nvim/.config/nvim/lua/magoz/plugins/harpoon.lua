cb45f0b14d06df5f2e57cdd23bc3a7573d6fd6f4
magoz <apps@magoz.is>
2024-01-21 11:13:35 +0100

    harpoon 2

--- nvim/.config/nvim/lua/magoz/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/magoz/plugins/harpoon.lua
@@ -1,58 +1,45 @@
 return {
 	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
+
 		require("which-key").register({ ["<leader>h"] = { name = "Harpoon" } })
 
-		vim.keymap.set(
-			"n",
-			"<leader>ha",
-			"<cmd>lua require('harpoon.mark').add_file()<cr>",
-			{ desc = "Add file to harpoon" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>hl",
-			"<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
-			{ desc = "Toggle Harpoon menu" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>hn",
-			"<cmd>lua require('harpoon.ui').nav_next()<cr>",
-			{ desc = "Go to next harpoon mark" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>hp",
-			"<cmd>lua require('harpoon.ui').nav_prev()<cr>",
-			{ desc = "Go to previous harpoon mark" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>h1",
-			"<cmd>lua require('harpoon.ui').nav_file(1)<cr>",
-			{ desc = "Go to harpoon mark 1" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>h2",
-			"<cmd>lua require('harpoon.ui').nav_file(2)<cr>",
-			{ desc = "Go to harpoon mark 2" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>h3",
-			"<cmd>lua require('harpoon.ui').nav_file(3)<cr>",
-			{ desc = "Go to harpoon mark 3" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>h4",
-			"<cmd>lua require('harpoon.ui').nav_file(4)<cr>",
-			{ desc = "Go to harpoon mark 4" }
-		)
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():append()
+		end, { desc = "Add file to harpoon" })
+
+		vim.keymap.set("n", "<leader>hl", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Toggle Harpoon menu" })
+
+		vim.keymap.set("n", "<leader>hn", function()
+			harpoon:list():prev({ ui_nav_wrap = true })
+		end, { desc = "Go to previous harpoon" })
+
+		vim.keymap.set("n", "<leader>hp", function()
+			harpoon:list():next({ ui_nav_wrap = true })
+		end, { desc = "Go to next harpoon" })
+
+		vim.keymap.set("n", "<leader>1", function()
+			harpoon:list():select(1)
+		end, { desc = "Go to harpoon 1" })
+
+		vim.keymap.set("n", "<leader>2", function()
+			harpoon:list():select(2)
+		end, { desc = "Go to harpoon 1" })
+
+		vim.keymap.set("n", "<leader>3", function()
+			harpoon:list():select(3)
+		end, { desc = "Go to harpoon 3" })
+
+		vim.keymap.set("n", "<leader-4>", function()
+			harpoon:list():select(4)
+		end, { desc = "Go to harpoon 4" })
 	end,
 }
