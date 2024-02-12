582ba36250743caa9a31d3c692af8292e05806b6
magoz <apps@magoz.is>
2024-02-12 08:50:12 +0100

    add apps to install

--- install-apps.sh
+++ install-apps.sh
@@ -6,19 +6,20 @@ cd "$(dirname "$0")" || exit
 # Disable Brew telemetry
 brew analytics off
 
-# -------------------------------------- 
+# --------------------------------------
 # -----------  INSTALL APPS ------------
-# -------------------------------------- 
+# --------------------------------------
 echo "🍺 Installing apps with Homebrew"
 brew install \
-  keyboard-maestro \
-  raycast \
-  amethyst \
-  istat-menus \
-  bartender \
-  appcleaner \
-  postico \
-  medis \
-  telegram \
-  spotify \
-  fantastical
+	keyboard-maestro \
+	raycast \
+	amethyst \
+	istat-menus \
+	bartender \
+	appcleaner \
+	postico \
+	medis \
+	telegram \
+	spotify \
+	todoist \
+	fantastical
