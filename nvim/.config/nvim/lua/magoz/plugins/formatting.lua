1813e4657b53777384749b5d37f074a31e6c36ed
magoz <apps@magoz.is>
2024-06-28 12:38:58 +0200

    comment out shfmt

--- nvim/.config/nvim/lua/magoz/plugins/formatting.lua
+++ nvim/.config/nvim/lua/magoz/plugins/formatting.lua
@@ -18,7 +18,7 @@ return {
 				markdown = { "prettier" },
 				graphql = { "prettier" },
 				lua = { "stylua" },
-				sh = { "shfmt" },
+				-- sh = { "shfmt" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
