c4b13a12008392700b7d0dbd302d0c031ec74e3b
magoz <apps@magoz.is>
2024-06-28 18:25:21 +0200

    improve telescope

--- nvim/.config/nvim/lua/magoz/plugins/telescope.lua
+++ nvim/.config/nvim/lua/magoz/plugins/telescope.lua
@@ -8,6 +8,20 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local select_one_or_multi = function(prompt_bufnr)
+			local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+			local multi = picker:get_multi_selection()
+			if not vim.tbl_isempty(multi) then
+				require("telescope.actions").close(prompt_bufnr)
+				for _, j in pairs(multi) do
+					if j.path ~= nil then
+						vim.cmd(string.format("%s %s", "edit", j.path))
+					end
+				end
+			else
+				require("telescope.actions").select_default(prompt_bufnr)
+			end
+		end
 
 		telescope.setup({
 			defaults = {
@@ -39,7 +53,8 @@ return {
 						["<Up>"] = actions.cycle_history_prev,
 						["<C-j>"] = actions.move_selection_next,
 						["<C-k>"] = actions.move_selection_previous,
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						-- ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<CR>"] = select_one_or_multi,
 					},
 
 					n = {
