0c4c452c441cdc06a0f4a49ded7ebd9aa60e82db
magoz <apps@magoz.is>
2024-10-21 23:43:29 +0200

    fix eslint to work without flat files

--- nvim/.config/nvim/lua/magoz/plugins/linting.lua
+++ nvim/.config/nvim/lua/magoz/plugins/linting.lua
@@ -11,16 +11,24 @@ return {
 			sh = { "shellcheck" },
 		}
 
+		lint.linters.eslint.cmd = "env"
+		lint.linters.eslint.args = {
+			"ESLINT_USE_FLAT_CONFIG=false", -- Remove this when we migrate to flat config
+			"eslint",
+			"--format",
+			"json",
+			"--stdin",
+			"--stdin-filename",
+			function()
+				return vim.api.nvim_buf_get_name(0)
+			end,
+		}
+
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
-				-- -- For monorepos
-				-- -- https://github.com/mfussenegger/nvim-lint/issues/482#issuecomment-1999185606
-				-- local get_clients = vim.lsp.get_clients or vim.lsp.get_active_clients
-				-- local client = get_clients({ bufnr = 0 })[1] or {}
-				-- lint.try_lint(nil, { cwd = client.root_dir })
 				lint.try_lint()
 			end,
 		})
@@ -28,5 +36,26 @@ return {
 		vim.keymap.set("n", "<leader>al", function()
 			lint.try_lint()
 		end, { desc = "Lint current file" })
+
+		-- Add a command to check ESLint output directly
+		vim.api.nvim_create_user_command("LintInfo", function()
+			local current_file = vim.fn.expand("%:p")
+			print("Running ESLint on " .. current_file)
+			local cwd = vim.fn.getcwd()
+			local cmd = string.format(
+				"cd %s && ESLINT_USE_FLAT_CONFIG=false pnpx eslint %s",
+				vim.fn.shellescape(cwd),
+				vim.fn.shellescape(current_file)
+			)
+			local output = vim.fn.system(cmd)
+			print(output)
+		end, {})
+
+		-- Add a command to check nvim-lint's ESLint configuration
+		vim.api.nvim_create_user_command("LintConfig", function()
+			print("ESLint configuration:")
+			print("Command: " .. vim.inspect(lint.linters.eslint.cmd))
+			print("Arguments: " .. vim.inspect(lint.linters.eslint.args))
+		end, {})
 	end,
 }
