d333086e53f8b2b0a6aff854925b8bf497cc2e2d
magoz <apps@magoz.is>
2025-06-08 09:11:42 +0200

    update keys, remove markdown

--- nvim/.config/nvim/lua/magoz/plugins/avante.lua
+++ nvim/.config/nvim/lua/magoz/plugins/avante.lua
@@ -8,10 +8,20 @@ return {
 		-- for example
 		provider = "claude",
 		claude = {
+			api_key_name = "AVANTE_ANTHROPIC_API_KEY",
 			endpoint = "https://api.anthropic.com",
 			model = "claude-3-5-sonnet-20241022",
 			temperature = 0,
 			max_tokens = 4096,
+			openai = {
+				api_key_name = "AVANTE_OPENAI_API_KEY",
+				endpoint = "https://api.openai.com/v1",
+				model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+				timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+				temperature = 0,
+				max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+				--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+			},
 		},
 	},
 	build = "make",
@@ -43,13 +53,13 @@ return {
 				},
 			},
 		},
-		{
-			-- Make sure to set this up properly if you have lazy=true
-			"MeanderingProgrammer/render-markdown.nvim",
-			opts = {
-				file_types = { "markdown", "Avante" },
-			},
-			ft = { "markdown", "Avante" },
-		},
+		-- {
+		-- 	-- Make sure to set this up properly if you have lazy=true
+		-- 	"MeanderingProgrammer/render-markdown.nvim",
+		-- 	opts = {
+		-- 		file_types = { "markdown", "Avante" },
+		-- 	},
+		-- 	ft = { "markdown", "Avante" },
+		-- },
 	},
 }
