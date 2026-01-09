a0eda5394aea58b4a80bacea136d2156569a8759
magoz <apps@magoz.is>
2026-01-09 13:53:30 +0100

    fix formatting json

--- nvim/.config/nvim/lua/magoz/plugins/formatting.lua
+++ nvim/.config/nvim/lua/magoz/plugins/formatting.lua
@@ -14,6 +14,7 @@ return {
 				css = { "prettier" },
 				html = { "prettier" },
 				json = { "prettier" },
+				jsonc = { "prettier" },
 				rust = { "rustfmt" },
 				yaml = { "prettier" },
 				markdown = { "prettier" },
@@ -28,7 +29,7 @@ return {
 					return
 				end
 				return {
-					timeout_ms = 500,
+					timeout_ms = 1000,
 					lsp_format = "fallback",
 				}
 			end,
